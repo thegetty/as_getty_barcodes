3a826725b697b4169d830fe3fbc1d9503cf8db2d
James Bullen <jambun@gmail.com>
2022-11-12 12:02:23 +1100

    tidy backend and get rid of temp file

--- backend/model/getty_barcodes.rb
+++ backend/model/getty_barcodes.rb
@@ -1,60 +1,32 @@
 class GettyBarcode
+  def self.generate(data, opts = {})
+    opts[:format] ||= 'png'
+    opts[:dpi] ||= 200
+    opts[:dot_size_px] ||= 8
 
-  def self.generate(data)
     bc = org.krysalis.barcode4j.impl.datamatrix.DataMatrixBean.new()
 
-    dpi = 200;
-
-    # makes a dot/module exactly eight pixels
-    bc.setModuleWidth(org.krysalis.barcode4j.tools.UnitConv.in2mm(8.0 / dpi));
-    bc.doQuietZone(false);
+    bc.setModuleWidth(org.krysalis.barcode4j.tools.UnitConv.in2mm(opts[:dot_size_px].to_f / opts[:dpi].to_f));
+    # bc.doQuietZone(false); # true by default adds a white border
     bc.setShape(org.krysalis.barcode4j.impl.datamatrix.SymbolShapeHint::FORCE_SQUARE);
 
-
-    antiAlias = false
+    anti_alias = false
     orientation = 0
-
-    canvas = org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider.new(dpi,
-                                                                           java.awt.image.BufferedImage::TYPE_BYTE_BINARY,
-                                                                           antiAlias,
-                                                                           orientation)
+    canvas = org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider.new(
+                 opts[:dpi],
+                 java.awt.image.BufferedImage::TYPE_BYTE_BINARY,
+                 anti_alias,
+                 orientation)
 
     bc.generateBarcode(canvas, data)
-
     canvas.finish()
 
-    bufimg = canvas.getBufferedImage()
-
-    output = java.io.ByteArrayOutputStream.new
-
     begin
-      javax.imageio.ImageIO.write(bufimg, "png", output)
-
-      tmp = ASUtils.tempfile("barcode.png")
-
-# this works
-#      file = java.io.FileOutputStream.new("barcode.png")
-#      output.writeTo(file)
-
-      output.writeTo(tmp)
-
-
-
-      # output_bytes = output.to_byte_array
-      # return DisplayCopyObj.new(java.io.ByteArrayInputStream.new(output_bytes),
-      #                           output_bytes.length,
-      #                           Digest::SHA256.hexdigest(output_bytes.to_ary.pack('C*')),
-      #                          'image/jpeg')
+      img_output = java.io.ByteArrayOutputStream.new
+      javax.imageio.ImageIO.write(canvas.getBufferedImage(), opts[:format], img_output)
+      java.io.ByteArrayInputStream.new(img_output.toByteArray()).to_io.to_enum
     ensure
-      output.close
+      img_output.close
     end
-
-#    {:neat => 'one'}
-
-#    output
-    tmp.flush
-    tmp.rewind
-    tmp
   end
-
 end
