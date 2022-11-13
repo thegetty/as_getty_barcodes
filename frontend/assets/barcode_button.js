52cbd4422a9cd65566298b7fc913f3340ec58f7d
James Bullen <jambun@gmail.com>
2022-11-13 16:34:27 +1100

    make the barcode button prefix-safe

--- frontend/assets/barcode_button.js
+++ frontend/assets/barcode_button.js
@@ -4,7 +4,8 @@
         const data = $('label[for=archival_object_ref_id_]').parent().find('.identifier-display').text();
 
         if (name && data) {
-            const bbut = '<a id="gettyBarcodeButton" class="btn btn-sm btn-default" href="/plugins/getty_barcode?data=' + data + '&name=' + name + '">Barcode</a>';
+            const url = AS.app_prefix('/plugins/getty_barcode?data=' + data + '&name=' + name);
+            const bbut = '<a id="gettyBarcodeButton" class="btn btn-sm btn-default" href="' + url + '">Barcode</a>';
             $('.record-toolbar > .btn-toolbar > .btn-group').prepend(bbut);
         }
     });
