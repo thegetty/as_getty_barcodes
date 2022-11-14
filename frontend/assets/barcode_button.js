8c9084a75e0438ad7bcae2ecaddb226629b91d8c
James Bullen <jambun@gmail.com>
2022-11-14 11:24:43 +1100

    display barcode because maybe that's nice

--- frontend/assets/barcode_button.js
+++ frontend/assets/barcode_button.js
@@ -5,8 +5,11 @@
 
         if (name && data) {
             const url = AS.app_prefix('/plugins/getty_barcode?data=' + data + '&name=' + name);
-            const bbut = '<a id="gettyBarcodeButton" class="btn btn-sm btn-default" href="' + url + '">Barcode</a>';
+            const bbut = '<a id="getty-barcode-button" class="btn btn-sm btn-default" href="' + url + '">Barcode</a>';
             $('.record-toolbar > .btn-toolbar > .btn-group').prepend(bbut);
+
+            const bimg = '<div class="pull-right"><a href="' + url + '" title="Click to download"><img id="getty-barcode-image" style="height:50px;" src="' + url + '"/></a></div>';
+            $('.record-pane').prepend(bimg);
         }
     });
 }(window));
