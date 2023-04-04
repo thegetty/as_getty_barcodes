d3f0a8df66521f172a82fbf8071843f2972ff9c6
James Bullen <jambun@gmail.com>
2023-04-04 12:23:28 +1000

    fix javascript when not on an ao page

--- frontend/assets/getty_barcode.js
+++ frontend/assets/getty_barcode.js
@@ -1,17 +1,20 @@
 (function(exports) {
     $(document).on("loadedrecordform.aspace", function(event, $pane) {
-        const name = $('label[for=archival_object_component_id_]').parent().find('div').text() || document.location.hash.split('::')[1];
-        const data = $('label[for=archival_object_ref_id_]').parent().find('.identifier-display').text();
-        const text = document.location.hash.split('::')[1].split('_')[2];
+        const loc_hash = document.location.hash.split('::');
+        if (loc_hash.length > 1) {
+            const name = $('label[for=archival_object_component_id_]').parent().find('div').text() || loc_hash[1];
+            const data = $('label[for=archival_object_ref_id_]').parent().find('.identifier-display').text();
+            const text = loc_hash[1].split('_')[2];
 
-        if (data) {
-            const url = AS.app_prefix('/plugins/getty_barcode?data=' + data + '&name=barcode_' + name + '&text=' + text);
-            const bbut = '<a id="getty-barcode-button" class="btn btn-sm btn-default" href="' + url + '">Barcode</a>';
-            $('.record-toolbar > .btn-toolbar > .btn-group').prepend(bbut);
+            if (data) {
+                const url = AS.app_prefix('/plugins/getty_barcode?data=' + data + '&name=barcode_' + name + '&text=' + text);
+                const bbut = '<a id="getty-barcode-button" class="btn btn-sm btn-default" href="' + url + '">Barcode</a>';
+                $('.record-toolbar > .btn-toolbar > .btn-group').prepend(bbut);
 
-            // show the barcode in the title area. disable for now. not sure if it's required
-            // const bimg = '<div class="pull-right"><a href="' + url + '" title="Click to download"><img id="getty-barcode-image" style="height:50px;" src="' + url + '"/></a></div>';
-            // $('.record-pane').prepend(bimg);
+                // show the barcode in the title area. disable for now. not sure if it's required
+                // const bimg = '<div class="pull-right"><a href="' + url + '" title="Click to download"><img id="getty-barcode-image" style="height:50px;" src="' + url + '"/></a></div>';
+                // $('.record-pane').prepend(bimg);
+            }
         }
     });
 }(window));
